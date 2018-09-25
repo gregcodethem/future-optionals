6343d868a95378b7a240f2cd6fe315c6728f3e41
Greg Shepley <gregshepley@yahoo.co.uk>
2018-09-25 16:18:29 +0100

    Split out unit tests into two files

---
+++ tasks/tests/test_models.py
@@ -0,0 +1,32 @@
+from tasks.models import Match, Task
+from django.test import TestCase
+
+
+class TaskAndMatchModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        task = Task()
+        task.save()
+
+        first_match = Match()
+        first_match.text = 'The first match'
+        first_match.task = task
+        first_match.save()
+
+        second_match = Match()
+        second_match.text = 'The second match'
+        second_match.task = task
+        second_match.save()
+
+        saved_task = Task.objects.first()
+        self.assertEqual(saved_task, task)
+
+        saved_matches = Match.objects.all()
+        self.assertEqual(saved_matches.count(), 2)
+
+        first_saved_match = saved_matches[0]
+        second_saved_match = saved_matches[1]
+        self.assertEqual(first_saved_match.text, 'The first match')
+        self.assertEqual(first_saved_match.task, task)
+        self.assertEqual(second_saved_match.text, 'The second match')
+        self.assertEqual(second_saved_match.task, task)
